8d04c041c0fcf9561d3c47b1a5fa5c8fe9325f93
JudyVue <judyvue87@gmail.com>
2017-03-21 10:00:34 -0700

    mvp but needs better css

--- main.js
+++ main.js
@@ -6,27 +6,31 @@
 
 const main = () => {
   let count = 1;
+
   pokemon.fetchData(count);
+
   $('h3').hide();
 
+  //next button click handler
   $('.next').on('click', (e) => {
     e.preventDefault();
     $('h3').hide();
-    $('.pokemon-container td').empty();
+    $('.pokemon-container tr').not('.header').empty();
     count+=5;
     pokemon.fetchData(count);
+
   });
 
+  //prev button click handler
   $('.prev').on('click', (e) => {
     e.preventDefault();
     if(count <= 1){
       $('h3').show();
     } else {
       count-=5;
-      $('.pokemon-container td').empty();
+      $('.pokemon-container tr').not('.header').empty();
       pokemon.fetchData(count);
     }
-
   });
 };
 
