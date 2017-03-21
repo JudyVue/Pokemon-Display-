e85ec358c23897350db97097f1a58acff71bc9bf
JudyVue <judyvue87@gmail.com>
2017-03-21 08:09:52 -0700

    got prev button working

--- main.js
+++ main.js
@@ -7,15 +7,26 @@
 const main = () => {
   let count = 1;
   pokemon.fetchData(count);
+  $('h3').hide();
 
   $('.next').on('click', (e) => {
     e.preventDefault();
+    $('h3').hide();
     $('.pokemon-container td').empty();
-
-    console.log('hit??');
-
     count+=5;
     pokemon.fetchData(count);
+  });
+
+  $('.prev').on('click', (e) => {
+    e.preventDefault();
+    if(count <= 1){
+      $('h3').show();
+    } else {
+      count-=5;
+      $('.pokemon-container td').empty();
+      pokemon.fetchData(count);
+    }
+
   });
 };
 
